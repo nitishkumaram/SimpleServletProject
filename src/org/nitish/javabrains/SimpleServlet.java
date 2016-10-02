0b8a4f4bdbd2704fe306d0c759cee525fdb1b5d5
Nitish Kumar <jointonitish@gmail.com>
2016-10-02 21:03:22 +0530

    1st Commit
    1st Commit

---
+++ src/org/nitish/javabrains/SimpleServlet.java
@@ -0,0 +1,24 @@
+package org.nitish.javabrains;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet("/SimpleServletPath")
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.print("Hello from get method. ");
+		// TODO Auto-generated method stub
+	}
+}
